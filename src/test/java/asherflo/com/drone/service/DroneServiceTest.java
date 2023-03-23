52db7dd1d6d65b2ae8057da489dbb975c4d60cdb
Asherflo <aniyikayetemitopeflorence@gmail.com>
2023-03-23 17:39:22 +0100

    To load drone Junit Test

--- src/test/java/asherflo/com/drone/service/DroneServiceTest.java
+++ src/test/java/asherflo/com/drone/service/DroneServiceTest.java
@@ -1,9 +1,10 @@
 package asherflo.com.drone.service;
 
-import asherflo.com.drone.dto.response.LoadDroneResponse;
 import asherflo.com.drone.dto.request.DroneRegistrationRequest;
-import asherflo.com.drone.dto.response.DroneResponse;
 import asherflo.com.drone.dto.request.LoadDroneRequest;
+import asherflo.com.drone.dto.response.DroneResponse;
+import asherflo.com.drone.dto.response.LoadDroneResponse;
+import asherflo.com.drone.exceptions.DroneException;
 import asherflo.com.drone.model.enums.DroneModel;
 import asherflo.com.drone.model.enums.DroneState;
 import org.junit.jupiter.api.DisplayName;
@@ -17,30 +18,29 @@ class DroneServiceTest {
     @Autowired
     private DroneService droneService;
 
-//    @Test
-//    @DisplayName("Register a drone")
-//    void testThatADroneCanBeRegister(){
-//        DroneRegistrationRequest droneRegistrationRequest = new DroneRegistrationRequest();
-//        droneRegistrationRequest.setSerialNumber("BIG023");
-//        droneRegistrationRequest.setDroneModel(DroneModel.LIGHTWEIGHT);
-//        droneRegistrationRequest.setWeightLimit(386);
-//        droneRegistrationRequest.setBatteryCapacity("95%");
-//        droneRegistrationRequest.setDroneState(DroneState.LOADING);
-//        DroneResponse response = droneService.registerDrone(droneRegistrationRequest);
-//        assertNotNull(response);assertNotNull
+    @Test
+    @DisplayName("Register a drone")
+    void testThatADroneCanBeRegister() throws DroneException {
+        DroneRegistrationRequest droneRegistrationRequest = new DroneRegistrationRequest();
+        droneRegistrationRequest.setSerialNumber("BIGY023");
+        droneRegistrationRequest.setDroneModel(DroneModel.LIGHTWEIGHT);
+        droneRegistrationRequest.setBatteryCapacity("95%");
+        droneRegistrationRequest.setDroneState(DroneState.LOADING);
+        DroneResponse response = droneService.registerDrone(droneRegistrationRequest);
+        assertNotNull(response);
     }
-//    @Test
-//    @DisplayName("To load a drone")
-//    void testThatADroneCanLoadedWithMedication(){
-//       LoadDroneRequest loadDroneRequest = new LoadDroneRequest();
-//       loadDroneRequest.setSerialNumber("QWRTYU567");
-//       loadDroneRequest.setName("Plaza");
-//       loadDroneRequest.setImage("IMG234");
-//       loadDroneRequest.setWeight(354);
-//      LoadDroneResponse response = droneService.loadDrone(loadDroneRequest);
-//      assertNotNull(response);
+    @Test
+    @DisplayName("To load a drone")
+    void testThatLoadDroneWithMedication(){
+        LoadDroneRequest loadRequest = new LoadDroneRequest();
+        loadRequest.setName("Confident");
+        loadRequest.setCode("WER234");
+        loadRequest.setSerialNumber("WEyu798");
+        loadRequest.setWeight(239.30);
+        LoadDroneResponse loadDroneResponse = new LoadDroneResponse();
+        loadDroneResponse.setMessage("Drone loaded successfully");
+        assertNotNull(loadDroneResponse);
 
-
-
-//   }
+    }
+}
 
